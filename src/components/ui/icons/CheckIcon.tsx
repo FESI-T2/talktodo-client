07a929b532d606b778242e3213d034bba62bfcc7
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-07-10 21:09:14 +0900

    style: 카카오 버튼 스타일 추가

--- src/components/ui/icons/CheckIcon.tsx
+++ src/components/ui/icons/CheckIcon.tsx
@@ -1,7 +1,8 @@
-export default function CheckIcon() {
+function CheckIcon() {
   return (
     <svg width='17' height='16' viewBox='0 0 17 16' fill='none' xmlns='http://www.w3.org/2000/svg'>
       <path d='M2.95654 7L7.45654 11.5L14.9565 4' stroke='#0F172A' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' />
     </svg>
   );
 }
+export default CheckIcon;
