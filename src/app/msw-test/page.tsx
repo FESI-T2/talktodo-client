bf96220a26b51c4fd6b59cf70b11a2886f7ab420
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-07-15 20:36:36 +0900

    test: 테스트용 컴포넌트 및 페이지 개발

---
+++ src/app/msw-test/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import MSWTestComponent from '@/components/MSWTestComponent';
+
+const MSWTestPage = () => {
+  return (
+    <div className='min-h-screen bg-gray-50'>
+      <MSWTestComponent />
+    </div>
+  );
+};
+
+export default MSWTestPage;
---
+++ src/app/mutation-test/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import GlobalErrorTestComponent from '@/components/test/GlobalErrorTestComponent';
+
+const ErrorTestPage = () => {
+  return (
+    <div className='min-h-screen bg-gray-50'>
+      <GlobalErrorTestComponent />
+    </div>
+  );
+};
+
+export default ErrorTestPage;
