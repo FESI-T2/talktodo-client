05127db11d10a8758278d249fb0d4c618d125e49
minjunpark <mj9457@naver.com>
2025-07-16 17:00:40 +0900

    style: 디자인 시스템 구현(font,color)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,35 @@
 export default function Home() {
-  return <main>여긴 메인</main>;
+  return (
+    <main>
+      여긴 메인
+      <p className='font-count'>Count</p>
+      <p>---------------------------------------------</p>
+      <p className='font-title1-bold'>Title1 bold</p>
+      <p className='font-title1-semibold'>Title1 semibold</p>
+      <p className='font-title2-bold'>Title2 bold</p>
+      <p className='font-title2-semibold'>Title2 semibold</p>
+      <p className='font-title3-bold'>Title3 bold</p>
+      <p className='font-title3-semibold'>Title3 semibold</p>
+      <p>---------------------------------------------</p>
+      <p className='font-body1-bold'>Body1 Bold</p>
+      <p className='font-body1-semibold'>Body1 semibold</p>
+      <p className='font-body1-medium'>Body1 medium</p>
+      <p className='font-body1-regular'>Body1 regular</p>
+      <p>---------------------------------------------</p>
+      <p className='font-body2-bold'>Body2 Bold</p>
+      <p className='font-body2-semibold'>Body2 semibold</p>
+      <p className='font-body2-medium'>Body2 medium</p>
+      <p className='font-body2-regular'>Body2 regular</p>
+      <p>---------------------------------------------</p>
+      <p className='font-body3-bold'>Body3 Bold</p>
+      <p className='font-body3-semibold'>Body3 semibold</p>
+      <p className='font-body3-medium'>Body3 medium</p>
+      <p className='font-body3-regular'>Body3 regular</p>
+      <p>---------------------------------------------</p>
+      <p className='font-caption-bold'>Caption bold</p>
+      <p className='font-caption-semibold'>Caption semibold</p>
+      <p className='font-caption-medium'>Caption medium</p>
+      <p className='font-caption-regular'>Caption regular</p>
+    </main>
+  );
 }
