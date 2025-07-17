60f2146b0a17f8dbaa3206cb8a6dd15df47a4a2d
kr-younghoon <sullanta0802@gmail.com>
2025-07-17 16:50:41 +0900

    feat: LabelPriority 컴포넌트 추가
    우선순위 스타일과 크기를 설정할 수 있는 LabelPriority 컴포넌트를 추가했습니다.
    이제 스토리북에서 다양한 우선순위와 크기를 테스트할 수 있습니다.

--- src/components/common/LabelPriority/PriorityDot.tsx
+++ src/components/common/LabelPriority/PriorityDot.tsx
@@ -4,10 +4,8 @@ interface PriorityDotProps {
 
 export default function PriorityDot({ color }: PriorityDotProps) {
   return (
-    <>
-      <svg width='11' height='11' viewBox='0 0 11 11' fill='none' xmlns='http://www.w3.org/2000/svg'>
-        <circle cx='5.5' cy='5.5' r='5.5' fill={color} />
-      </svg>
-    </>
+    <svg width='11' height='11' viewBox='0 0 11 11' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <circle cx='5.5' cy='5.5' r='5.5' fill={color} />
+    </svg>
   );
 }
