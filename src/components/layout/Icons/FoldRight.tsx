44a56bcf8a20d48c20a3c7a10953d25ee3099d17
mj9457 <mj9457@naver.com>
2025-07-19 04:01:31 +0900

    feat: 새로운 아이콘 컴포넌트 추가
    Flag, FoldLeft, FoldRight, Home, MainLogo, Plus, Profile 아이콘 추가

---
+++ src/components/layout/Icons/FoldRight.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const SvgIconFoldRight = () => {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none'>
+      <g clip-path='url(#clip0_662_8942)'>
+        <path
+          d='M3.85268 11.8564L22 11.8564M22 11.8564L19 14.8564M22 11.8564L19 8.85644'
+          stroke='#BBB8C3'
+          stroke-width='2'
+          stroke-linecap='round'
+          stroke-linejoin='round'
+        />
+        <path d='M3.8538 18L13.7109 18' stroke='#BBB8C3' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' />
+        <path d='M3.8538 5.85644L13.7109 5.85645' stroke='#BBB8C3' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' />
+      </g>
+      <defs>
+        <clipPath id='clip0_662_8942'>
+          <rect width='24' height='24' fill='white' transform='matrix(0 1 1 0 0 0)' />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+
+export default SvgIconFoldRight;
---
+++ src/components/layout/Icons/Plus.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SvgIconPlus = () => {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' width='21' height='20' viewBox='0 0 21 20' fill='none'>
+      <path d='M3.5 10H17' stroke='#8F3FFF' stroke-width='2' stroke-linecap='round' />
+      <path d='M10.25 16.75V3.25' stroke='#8F3FFF' stroke-width='2' stroke-linecap='round' />
+    </svg>
+  );
+};
+
+export default SvgIconPlus;
