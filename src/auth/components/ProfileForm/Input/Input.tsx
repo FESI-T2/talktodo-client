1b735edb9ca84a681f54949b815cf960518a6d2c
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-07-22 00:35:24 +0900

    feat: input 컴포넌트 개발

---
+++ src/auth/components/ProfileForm/Input/Input.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  className?: string;
+}
+
+const Input = ({ className, ...props }: InputProps) => {
+  return <input className={`font-body2-regular bg-[var(--color-gray-100)] rounded-xl text-body2 p-3 w-full ${className}`} {...props} />;
+};
+
+export default Input;
