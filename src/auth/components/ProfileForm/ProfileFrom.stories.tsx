376f022b51378c11f98b244e4a545a83a4d4cdff
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-07-22 16:32:05 +0900

    fix: 잘못된 컴포넌트명 스토리 수정

--- src/auth/components/ProfileForm/ProfileFrom.stories.tsx
+++ src/auth/components/ProfileForm/ProfileFrom.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/nextjs-vite';
+import { useRef } from 'react';
 
 import ProfileFrom from './ProfileFrom';
 
@@ -14,10 +15,12 @@ type Story = StoryObj<typeof ProfileFrom>;
 
 export const Default: Story = {
   render: () => {
+    const imageRef = useRef<HTMLInputElement>(null);
+
     return (
       <ProfileFrom>
         <ProfileFrom.Title>{'회원 가입'}</ProfileFrom.Title>
-        <ProfileFrom.ImageUpload />
+        <ProfileFrom.ProfileUpload imageRef={imageRef} />
         <ProfileFrom.Info content={'이름'} />
         <ProfileFrom.SocialInfo content={'이메일'} variant={'kakao'} />
         <ProfileFrom.Button />
