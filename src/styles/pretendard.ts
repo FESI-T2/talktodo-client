8a6ac2e96923095dce1ecfb2ce135101d62d1000
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-07-22 17:08:20 +0900

    chore: 번들 사이즈를 줄이기 위해 가변폰트 고정폰트로 수정

--- src/styles/pretendard.ts
+++ src/styles/pretendard.ts
@@ -1,7 +1,12 @@
 import localFont from 'next/font/local';
 
 const pretendard = localFont({
-  src: '../../public/fonts/PretendardVariable.woff2',
+  src: [
+    { path: '../../public/fonts/Pretendard-Regular.subset.woff2', weight: '400', style: 'normal' },
+    { path: '../../public/fonts/Pretendard-Medium.subset.woff2', weight: '500', style: 'normal' },
+    { path: '../../public/fonts/Pretendard-SemiBold.subset.woff2', weight: '600', style: 'normal' },
+    { path: '../../public/fonts/Pretendard-Bold.subset.woff2', weight: '700', style: 'normal' },
+  ],
   display: 'swap',
   variable: '--font-pretendard',
 });
