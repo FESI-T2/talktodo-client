d272261975269a932341e31a5fc020bc672ca790
kr-younghoon <sullanta0802@gmail.com>
2025-07-23 16:53:09 +0900

    fix: SVG 속성의 카멜 케이스 표기법으로 수정
    여러 SVG 파일에서 stroke-width, stroke-linecap 등의 속성을 카멜 케이스로 변경하여 일관성을 유지했습니다.

--- src/task/components/common/icons/checkIcon/CheckIcon.tsx
+++ src/task/components/common/icons/checkIcon/CheckIcon.tsx
@@ -20,14 +20,14 @@ export default function CheckIcon({ state = 'off', size = 'S' }: CheckIconProps)
 function IconCheckOffS() {
   return (
     <svg width='28' height='28' viewBox='0 0 28 28' fill='none' xmlns='http://www.w3.org/2000/svg'>
-      <rect x='1' y='1' width='26' height='26' rx='13' stroke='#AE7BF6' stroke-width='2' />
-      <g clip-path='url(#clip0_1067_15454)'>
+      <rect x='1' y='1' width='26' height='26' rx='13' stroke='#AE7BF6' strokeWidth='2' />
+      <g clipPath='url(#clip0_1067_15454)'>
         <path
           d='M8.89697 13.9997L12.5428 17.6455L19.8345 10.3539'
           stroke='#AE7BF6'
-          stroke-width='2.5'
-          stroke-linecap='round'
-          stroke-linejoin='round'
+          strokeWidth='2.5'
+          strokeLinecap='round'
+          strokeLinejoin='round'
         />
       </g>
       <defs>
@@ -42,14 +42,14 @@ function IconCheckOffS() {
 function IconCheckOffL() {
   return (
     <svg width='32' height='32' viewBox='0 0 32 32' fill='none' xmlns='http://www.w3.org/2000/svg'>
-      <rect x='1' y='1' width='30' height='30' rx='15' stroke='#AE7BF6' stroke-width='2' />
-      <g clip-path='url(#clip0_519_17930)'>
+      <rect x='1' y='1' width='30' height='30' rx='15' stroke='#AE7BF6' strokeWidth='2' />
+      <g clipPath='url(#clip0_519_17930)'>
         <path
           d='M10.168 15.9997L14.3346 20.1663L22.668 11.833'
           stroke='#AE7BF6'
-          stroke-width='2.5'
-          stroke-linecap='round'
-          stroke-linejoin='round'
+          strokeWidth='2.5'
+          strokeLinecap='round'
+          strokeLinejoin='round'
         />
       </g>
       <defs>
@@ -65,13 +65,13 @@ function IconCheckOnL() {
   return (
     <svg width='32' height='32' viewBox='0 0 32 32' fill='none' xmlns='http://www.w3.org/2000/svg'>
       <rect width='32' height='32' rx='16' fill='#E5D6FC' />
-      <g clip-path='url(#clip0_519_13433)'>
+      <g clipPath='url(#clip0_519_13433)'>
         <path
           d='M10.168 15.9997L14.3346 20.1663L22.668 11.833'
           stroke='#C8A5F9'
-          stroke-width='2.5'
-          stroke-linecap='round'
-          stroke-linejoin='round'
+          strokeWidth='2.5'
+          strokeLinecap='round'
+          strokeLinejoin='round'
         />
       </g>
       <defs>
@@ -87,13 +87,13 @@ function IconCheckOnS() {
   return (
     <svg width='28' height='28' viewBox='0 0 28 28' fill='none' xmlns='http://www.w3.org/2000/svg'>
       <rect width='28' height='28' rx='14' fill='#E5D6FC' />
-      <g clip-path='url(#clip0_1067_15450)'>
+      <g clipPath='url(#clip0_1067_15450)'>
         <path
           d='M8.89697 13.9997L12.5428 17.6455L19.8345 10.3539'
           stroke='#C8A5F9'
-          stroke-width='2.5'
-          stroke-linecap='round'
-          stroke-linejoin='round'
+          strokeWidth='2.5'
+          strokeLinecap='round'
+          strokeLinejoin='round'
         />
       </g>
       <defs>
