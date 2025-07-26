7ffe8640610325adff248aeb013ad3701c98fb88
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-07-27 01:57:39 +0900

    feat: 모달 컴포넌트 및 훅 개선

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 
 import ErrorBoundary from '@/components/error/ErrorBoundary';
+import ModalContainer from '@/components/modal/ModalContainer';
 import QueryProvider from '@/components/QueryProvider';
 import ToastContainer from '@/components/toast/ToastContainer';
 import MSWProvider from '@/mocks/MSWProvider';
@@ -15,8 +16,10 @@ export default function RootLayout({
     <html lang='en'>
       <body className={pretendard.className}>
         <div id='toast-root' />
+        <div id='modal-root' />
         <ErrorBoundary>
           <ToastContainer />
+          <ModalContainer />
           <MSWProvider>
             <QueryProvider>{children}</QueryProvider>
           </MSWProvider>
