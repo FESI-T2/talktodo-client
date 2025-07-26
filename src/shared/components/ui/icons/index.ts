aecea326e06c893b0c98e202dd02595ac86c2c28
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-07-27 05:31:23 +0900

    chore: 사용하지 않는 폴더 제거

--- src/shared/components/ui/icons/index.ts
+++
@@ -1,4 +0,0 @@
-export { AddWhiteIcon, AddBlackIcon } from './AddIcon';
-export { default as CheckIcon } from './CheckIcon';
-export { default as DeleteIcon } from './DeleteIcon';
-export { default as KakaoIcon } from './KakaoIcon';
