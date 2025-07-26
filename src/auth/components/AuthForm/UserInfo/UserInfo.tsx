43921bd62ac0a5d735c4ea84b1269ca28dfe20dc
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-07-27 05:11:51 +0900

    chore: placeholder 상수화 분리

--- src/auth/components/AuthForm/UserInfo/UserInfo.tsx
+++ src/auth/components/AuthForm/UserInfo/UserInfo.tsx
@@ -1,10 +1,11 @@
+import { PLACEHOLDER_TEXT } from '@/auth/constants/text';
 import Input from '@/shared/components/Input/Input';
 
 const UserInfo = ({ ...props }: React.HTMLAttributes<HTMLInputElement>) => {
   return (
     <div className='mb-6'>
       <h2 className='font-body3-semibold mb-3'>닉네임</h2>
-      <Input {...props} placeholder='닉네임을 최대 10자 이내로 입력해주세요' />
+      <Input {...props} placeholder={PLACEHOLDER_TEXT} />
     </div>
   );
 };
