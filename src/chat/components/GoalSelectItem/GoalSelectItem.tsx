11acd3b499fc59c97706016d7af18a3380b4a89c
mj9457 <mj9457@naver.com>
2025-07-29 04:16:31 +0900

    feat: 'TaskCount' 속성을 'taskCount'로 수정 및 MessageSendButton 컴포넌트를 버튼으로 변경
    - GoalSelectItem에 반응형 디자인 추가
    
    ISSUES CLOSED: open #71

--- src/chat/components/GoalSelectItem/GoalSelectItem.tsx
+++ src/chat/components/GoalSelectItem/GoalSelectItem.tsx
@@ -1,25 +1,27 @@
 'use client';
 
-import React from 'react';
-
 interface GoalSelectItemProps {
   GoalTitle: string;
-  TaskCount: number;
+  taskCount: number;
   active?: boolean;
   onClick?: () => void;
 }
 
-const GoalSelectItem = ({ GoalTitle, TaskCount, active = false, onClick }: GoalSelectItemProps) => {
+const GoalSelectItem = ({ GoalTitle, taskCount, active = false, onClick }: GoalSelectItemProps) => {
   return (
     <button
       aria-pressed={active}
       onClick={onClick}
-      className={`w-[524px] h-[90px] py-5 px-6 gap-2 flex flex-col items-start justify-center border-solid rounded-2xl cursor-pointer
+      className={`flex flex-col items-start justify-center border-solid rounded-2xl cursor-pointer
+        tb:w-[524px] tb:h-[90px] tb:py-5 tb:px-6 tb:gap-1 
+        w-[289px] h-[72px] p-4 gap-1
         ${active ? 'bg-purple-50 border-2 border-purple-500' : 'bg-white border border-gray-200'}
         ${active ? 'text-purple-500' : 'text-gray-900'}`}
     >
-      <p className={`font-body1-semibold`}>{GoalTitle}</p>
-      <p className={`${active ? 'text-purple-400' : 'text-gray-500'} font-body2-medium-tight`}>{TaskCount}개의 할일</p>
+      <p className={`tb:font-body1-semibold font-body2-semibold`}>{GoalTitle}</p>
+      <p className={`${active ? 'text-purple-400' : 'text-gray-500'} tb:font-body2-medium-tight font-body3-medium-tight`}>
+        {taskCount}개의 할일
+      </p>
     </button>
   );
 };
--- src/chat/components/MessageSendButton/MessageSendButton.tsx
+++ src/chat/components/MessageSendButton/MessageSendButton.tsx
@@ -2,16 +2,20 @@ import Send from '@/shared/components/Icons/Send/Send';
 
 interface MessageSendButtonProps {
   active?: boolean;
+  onClick: () => void;
 }
 
-const MessageSendButton = ({ active }: MessageSendButtonProps) => {
+const MessageSendButton = ({ active, onClick }: MessageSendButtonProps) => {
   return (
-    <div
+    <button
+      type='button'
+      aria-label='메시지 전송'
       className={`${active ? 'bg-gray-900' : 'bg-gray-300'}
   flex w-[56px] h-[56px] justify-center items-center rounded-full cursor-pointer`}
+      onClick={onClick}
     >
       <Send />
-    </div>
+    </button>
   );
 };
 
