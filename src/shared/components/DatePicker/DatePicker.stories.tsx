cf489afbb1086ab10edbe9c3e1ef359a1354c031
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-07-29 22:56:17 +0900

    chore: 스토리 에러 수정

--- src/shared/components/DatePicker/DatePicker.stories.tsx
+++ src/shared/components/DatePicker/DatePicker.stories.tsx
@@ -25,7 +25,7 @@ export const Range: Story = {
       console.log('DatePicker mounted with initial date:', date);
     }, [date]);
 
-    return <DatePicker mode='range' setDate={setDate} />;
+    return <DatePicker mode='range' setDate={setDate} closeSelector={() => {}} />;
   },
 };
 
@@ -37,6 +37,6 @@ export const Single: Story = {
       console.log('DatePicker mounted with initial date:', date);
     }, [date]);
 
-    return <DatePicker mode='single' setDate={setDate} />;
+    return <DatePicker mode='single' setDate={setDate} closeSelector={() => {}} />;
   },
 };
