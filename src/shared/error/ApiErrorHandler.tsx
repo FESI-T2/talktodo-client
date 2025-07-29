bde6e155ff60cfde409e4791ce660d79ce8108fd
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-07-29 17:06:11 +0900

    feat: toast 호출 부분 alert 호출로 변경

--- src/shared/error/ApiErrorHandler.tsx
+++ src/shared/error/ApiErrorHandler.tsx
@@ -1,31 +1,31 @@
 'use client';
 import { useEffect } from 'react';
 
-import { useToast } from '@/shared/hooks/useToast';
 import classifyAPIError from '@/shared/lib/error/classifyAPIError';
 import { CustomError } from '@/shared/lib/error/customError';
 import { processError, createErrorActions } from '@/shared/lib/error/handleError';
 
 import { Fallback, PageFallback } from './Fallback/FallBack';
+import { useAlert } from '../hooks/useAlert';
 
 interface ApiErrorHandlerProps {
   error: Error;
-  level: 'toast' | 'fallback' | 'page';
+  level: 'alert' | 'fallback' | 'page';
   onReset: () => void;
 }
 
 const ApiErrorHandler = ({ error, level, onReset }: ApiErrorHandlerProps) => {
-  const toast = useToast();
+  const { openAlert } = useAlert();
 
   if (error instanceof CustomError) {
     useEffect(() => {
-      processError(error, createErrorActions(toast.error));
+      processError(error, createErrorActions(openAlert));
     }, []);
   } else {
     throw classifyAPIError(error);
   }
 
-  if (level === 'toast') return null;
+  if (level === 'alert') return null;
 
   if (level === 'fallback') return <Fallback onReset={onReset} />;
 
