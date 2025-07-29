9719a8ff3156f996d850e4e5a8a7c5853cd8a6c5
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-07-29 17:14:31 +0900

    chore: 변경된 타입에 맞춰 props 수정

--- src/shared/provider/QueryProvider.tsx
+++ src/shared/provider/QueryProvider.tsx
@@ -27,7 +27,7 @@ export default function QueryProvider({ children }: { children: React.ReactNode 
 
   return (
     <QueryClientProvider client={queryClient}>
-      <ApiErrorBoundary level='toast'>{children}</ApiErrorBoundary>
+      <ApiErrorBoundary level='alert'>{children}</ApiErrorBoundary>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
