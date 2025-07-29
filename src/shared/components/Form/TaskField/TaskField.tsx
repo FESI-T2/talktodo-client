25b70766115b00f684101489ab4ae65638b0195c
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-07-29 22:53:15 +0900

    design: 모달 스타일 변경
    DateSelector 뷰포트 넘어갈 시 scroll 생성 및 길이 및 너비 수정

--- src/shared/components/Form/TaskField/TaskField.tsx
+++ src/shared/components/Form/TaskField/TaskField.tsx
@@ -1,17 +1,19 @@
 'use client';
+import { DateRange } from 'react-day-picker';
+
 import { Priority } from '@/shared/types/prioity';
-import { DateSelectorProps } from '@/shared/types/props';
 
 import DateSelector from '../../DateSelector/DateSelector';
 import Input from '../../Input/Input';
 import SelectPriority from '../../Select/SelectPriority';
 import SubText from '../SubText/SubText';
 
-type TaskFieldProps = DateSelectorProps &
-  React.HTMLAttributes<HTMLInputElement> & {
-    priority: Priority;
-    setPriority: React.Dispatch<React.SetStateAction<Priority>>;
-  };
+type TaskFieldProps = React.HTMLAttributes<HTMLInputElement> & {
+  priority: Priority;
+  setPriority: React.Dispatch<React.SetStateAction<Priority>>;
+  date: DateRange;
+  setDate: React.Dispatch<React.SetStateAction<DateRange>>;
+};
 
 const TaskField = ({ date, setDate, priority, setPriority, ...props }: TaskFieldProps) => {
   return (
@@ -21,7 +23,7 @@ const TaskField = ({ date, setDate, priority, setPriority, ...props }: TaskField
       </SubText>
       <div className='flex gap-4 '>
         <SubText text='날짜' className='w-1/2 '>
-          <DateSelector date={date} setDate={setDate} />
+          <DateSelector date={date} setDate={setDate} mode={'range'} />
         </SubText>
         <SubText text='우선 순위' className='w-1/2 '>
           <SelectPriority label='우선 순위' options={['중요', '보통', '낮음']} onSelect={setPriority} selectedValue={priority} />
