4b2111b025e534dcee54da397fe4594ab593cf18
kr-younghoon <sullanta0802@gmail.com>
2025-07-31 16:12:45 +0900

    feat: Dashboard 페이지 MainHeader 컴포넌트 추가

---
+++ src/app/(task)/dashboard/page.tsx
@@ -0,0 +1,22 @@
+import MainHeader from '@/task/components/layout/Header/MainHeader';
+import useDashboardTasks from '@/task/hooks/useDashboardTasks';
+
+export default function DashboardPage() {
+  const { data } = useDashboardTasks();
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
+  const {
+    result: { undoneCount, doneCount, totalCount, undoneTasks, doneTasks },
+  } = data;
+
+  console.log(undoneTasks, doneTasks);
+
+  return (
+    <div>
+      <MainHeader totalTodo={totalCount} IncompleteTodo={undoneCount} completedTodo={doneCount} />
+    </div>
+  );
+}
