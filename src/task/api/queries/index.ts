b3e7c406b1216ad46193f2c4449deffc2b3f3f5b
kr-younghoon <sullanta0802@gmail.com>
2025-08-03 04:53:46 +0900

    feat: useTasksByDate 및 useTasksById useQuery훅 추가
    작업 날짜 및 ID에 따라 작업을 가져오는 쿼리 훅을 추가하여 API 호출을 간소화했습니다.

---
+++ src/task/api/queries/index.ts
@@ -0,0 +1,2 @@
+export { default as useTasksByDate } from './useTasksByDate';
+export { default as useTaskById } from './useTasksById';
---
+++ src/task/api/queries/useTasksByDate.ts
@@ -0,0 +1,13 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { tasksKeys } from '../queryKeys/tasksKeys';
+import { fetchTasksByDate } from '../tasks';
+
+export default function useTasksByDate(date: string) {
+  return useQuery({
+    queryKey: tasksKeys.byDate(date).queryKey,
+    queryFn: () => fetchTasksByDate(date),
+    enabled: !!date,
+    staleTime: 1000 * 60 * 3,
+  });
+}
