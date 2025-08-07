430ad54543f08fc511d9a43f7008b5c409f3015d
kr-younghoon <sullanta0802@gmail.com>
2025-08-08 03:57:55 +0900

    chore: API 경로 조정 및 SuspenseQuery를 useQuery로 변경
    goalApi의 getAllGoal 메서드를 수정하여 개발 환경에 따라 API 경로를 조정하는 로직을 제거하고, useGetAllGoal 훅에서 useSuspenseQuery를 useQuery로 변경했습니다.

--- src/goal/lib/goalApi.ts
+++ src/goal/lib/goalApi.ts
@@ -2,11 +2,7 @@ import { GetAllGoal } from '@/goal/types/response/index';
 import APIBuilder from '@/shared/lib/api/apiBuilder';
 
 const goalApi = {
-  getAllGoal: () =>
-    APIBuilder.get(process.env.NODE_ENV === 'development' ? '/api/goal' : '/goal')
-      .withCredentials(true)
-      .build()
-      .call<GetAllGoal>(),
+  getAllGoal: () => APIBuilder.get('/goal').withCredentials(true).build().call<GetAllGoal>(),
 };
 
 export default goalApi;
