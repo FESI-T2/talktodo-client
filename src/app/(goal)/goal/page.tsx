f328f7256f3416a07237e71bf310ef992e622fac
kr-younghoon <sullanta0802@gmail.com>
2025-08-08 05:53:06 +0900

    feat: GoalPage 컴포넌트의 레이아웃 수정
    목표별 할 일 리스트 섹션의 레이아웃을 개선하여 가독성을 높였습니다.

--- src/app/(goal)/goal/page.tsx
+++ src/app/(goal)/goal/page.tsx
@@ -11,7 +11,7 @@ export default function GoalPage() {
   return (
     <div className='flex mt-20 max-w-[1120px] w-full flex-col items-start gap-10'>
       <div className='font-title2-bold text-gray-900'>목표별 할 일 리스트</div>
-      <div>
+      <div className='flex flex-row items-start content-start gap-5 self-stretch flex-wrap max-w-[1120px] w-full'>
         {data?.data?.result?.map((goal) => (
           <Fragment key={goal.goalId}>
             <GoalCard goal={goal} />
