cf80b5c877a3a225f16c4f3d76e6d33f3a8cda53
kr-younghoon <sullanta0802@gmail.com>
2025-08-10 11:38:02 +0900

    feat: API 엔드포인트를 v1로 업데이트
    목표 및 할일 관련 API 호출의 경로를 '/v1/'로 수정하여 버전 관리를 개선했습니다.

--- src/goal/lib/goalApi.ts
+++ src/goal/lib/goalApi.ts
@@ -11,19 +11,19 @@ import {
 } from '../types/response';
 
 const goalApi = {
-  getAllGoal: () => APIBuilder.get('/goal').withCredentials(true).build().call<ApiResponseGoalList>(),
+  getAllGoal: () => APIBuilder.get('/v1/goal').withCredentials(true).build().call<ApiResponseGoalList>(),
 
-  getGoalsWithTaskCount: () => APIBuilder.get('/goal/with-task-count').withCredentials(true).build().call<ApiResponseGoalWithCountList>(),
+  getGoalsWithTaskCount: () =>
+    APIBuilder.get('/v1/goal/with-task-count').withCredentials(true).build().call<ApiResponseGoalWithCountList>(),
 
-  createGoal: (goalName: string) => APIBuilder.post('/goal', { goalName }).withCredentials(true).build().call<ApiResponseGoal>(),
+  createGoal: (goalName: string) => APIBuilder.post('/v1/goal', { goalName }).withCredentials(true).build().call<ApiResponseGoal>(),
 
   updateGoal: (goalId: string, goalName: string) =>
-    APIBuilder.put(`/goal/${goalId}`, { goalName }).withCredentials(true).build().call<ApiResponseGoal>(),
+    APIBuilder.put(`/v1/goal/${goalId}`, { goalName }).withCredentials(true).build().call<ApiResponseGoal>(),
 
-  getGoalDetail: (goalId: string) => APIBuilder.get(`/goal/${goalId}`).withCredentials(true).build().call<ApiResponseGoalDetail>(),
+  getGoalDetail: (goalId: string) => APIBuilder.get(`/v1/goal/${goalId}`).withCredentials(true).build().call<ApiResponseGoalDetail>(),
 
-
-  deleteGoal: (goalId: string) => APIBuilder.delete(`/goal/${goalId}`).withCredentials(true).build().call<ApiResponseString>(),
+  deleteGoal: (goalId: string) => APIBuilder.delete(`/v1/goal/${goalId}`).withCredentials(true).build().call<ApiResponseString>(),
 };
 
 export default goalApi;
